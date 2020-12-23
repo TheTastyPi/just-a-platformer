169b370f311b973969ec4901cd578159a613068f
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-23 23:29:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const player = {
 	y: 50,
 	xv: 0,
 	yv: 0,
-	g: 5,
+	g: 20,
 	canJump: false,
 };
 
@@ -13,15 +13,15 @@ document.addEventListener("keydown", function(input){
 	switch(key) {
 		case "ArrowUp":
 		case "KeyW":
-			player.yv = -10;
+			player.yv = -40;
 			break;
 		case "ArrowLeft":
 		case "KeyA":
-			player.xv = -5;
+			player.xv = -20;
 			break;
 		case "ArrowRight":
 		case "KeyD":
-			player.xv = 5;
+			player.xv = 20;
 			break;
 			
 	}
@@ -31,9 +31,14 @@ var lastFrame = 0;
 function nextFrame(timeStamp) {
 	let dt = timeStamp - lastFrame;
 	lastFrame = timeStamp;
+	
 	player.x += player.xv * dt / 1000;
 	player.y += player.yv * dt / 1000;
+	
+	player.xv *= 0.5;
+	if (Math.abs(player.xv) < 5) player.xv = 0;
 	player.yv += player.g * dt / 1000;
+	
 	draw();
 	window.requestAnimationFrame(nextFrame);
 }
