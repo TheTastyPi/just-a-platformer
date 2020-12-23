19e2edfa00528f1bec4ae84e64e2cdd24225a8f6
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-23 23:04:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-var game = {}; // i'll add something here later
+var savePoint = 0; // i'll add something here later
+const player = {
+	x: 50,
+	y: 50,
+	xv: 0,
+	yv: 0,
+};
+var grav = 5;
+var lastFrame = 0;
 function nextFrame(timeStamp) {
+	let dt = timeStamp - lastFrame;
+	lastFrame = timeStamp;
+	player.x += player.xv * dt / 1000;
+	player.y += player.yv * dt / 1000;
+	player.xv -= 5;
 	draw();
+	window.requestAnimationFrame(nextFrame);
 }
 function draw() {
 	let screen = document.getElementById("gameScreen").getContext("2d");
-	// something to do something
+	screen.clearRect(0,0,"100%","100%");
+	
+	screen.fillStyle = "#FF0000";
+	screen.fillRect(player.x, player.y, 5, 5);
 }
 window.requestAnimationFrame(nextFrame);
