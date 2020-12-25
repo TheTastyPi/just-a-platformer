2939dc0e60c81d7f4f1ac4c5aca5b9f190a5ee30
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-25 17:02:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var playerSize = 20;
 var blockSize = 50;
 const player = {
-	spawnPoint: [4,7,0,0],
+	spawnPoint: [4,8,0,0],
 	levelCoord: [0,0],
 	get currentLevel() {return worldMap[player.levelCoord[0]][player.levelCoord[1]]},
 	x: 240,
