ddf43f779944234d30bcb3d964dd150445fe425b
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-26 23:30:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -236,8 +236,8 @@ function nextFrame(timeStamp) {
 			player.yv = 0;
 			if (((getBlockType(x2b,y1b) == 5 && getBlockType(x1b,y1b) == 5)
 			   || ((getBlockType(x2b,y1b) == 5 || getBlockType(x1b,y1b) == 5)
-			       && ((noHitbox.includes(getBlockType(x2b,y1b)) || !noHitbox.includes(getBlockType(x1b,y2b-1)))
-				   || (noHitbox.includes(getBlockType(x1b,y1b)) || !noHitbox.includes(getBlockType(x1b,y2b-1))))))
+			       && ((noHitbox.includes(getBlockType(x2b,y1b)) || !noHitbox.includes(getBlockType(x2b,y1b-1)))
+				   || (noHitbox.includes(getBlockType(x1b,y1b)) || !noHitbox.includes(getBlockType(x1b,y1b-1))))))
 			   && player.g < 0) player.yv = -player.g*3/4;
 			player.y = (y1b + 1) * blockSize;
 			if (player.g < 0 && player.yv <= 0) player.canJump = true;
@@ -247,7 +247,7 @@ function nextFrame(timeStamp) {
 			player.yv = 0;
 			if (((getBlockType(x2b,y2b) == 5 && getBlockType(x1b,y2b) == 5)
 			   || ((getBlockType(x2b,y2b) == 5 || getBlockType(x1b,y2b) == 5)
-			       && ((noHitbox.includes(getBlockType(x2b,y2b)) || !noHitbox.includes(getBlockType(x1b,y2b-1))) 
+			       && ((noHitbox.includes(getBlockType(x2b,y2b)) || !noHitbox.includes(getBlockType(x2b,y2b-1))) 
 				   || (noHitbox.includes(getBlockType(x1b,y2b)) || !noHitbox.includes(getBlockType(x1b,y2b-1))))))
 			   && player.g > 0) player.yv = -player.g*3/4;
 			player.y = y2b * blockSize - playerSize;
