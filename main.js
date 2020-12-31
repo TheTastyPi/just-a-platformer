25d94a2d11506b82ee13bf3409a8b67d99fd7ab3
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-31 11:55:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -395,28 +395,29 @@ function nextFrame(timeStamp) {
 		// level warp
 		if (isTouching("any",-2)) {
 			let coord = getCoord(-1);
-			let warpId = levels[player.currentLevel][coord[0]][coord[1]][1];
+			let warp = levels[player.currentLevel][coord[0]][coord[1]];
+			let warpId = warp[1];
 			if (x1 < 0) { // left
-				if (levels[player.currentLevel][coord[0]][coord[1]][2] != undefined) {
-					player.levelCoord = [coord[2],coord[3]];
+				if (warp[2] != undefined) {
+					player.levelCoord = [warp[2],warp[3]];
 				} else player.levelCoord[0]--;
 				player.x = levels[player.currentLevel].length * blockSize - playerSize;
 				player.y = blockSize*levels[player.currentLevel][levels[player.currentLevel].length-1].findIndex(x => x[0]==-1 && x[1]==warpId)+(y1+blockSize)%blockSize;
 			} else if (x2 > levels[player.currentLevel].length * blockSize) { // right
-				if (levels[player.currentLevel][coord[0]][coord[1]][2] != undefined) {
-					player.levelCoord = [coord[2],coord[3]];
+				if (warp[2] != undefined) {
+					player.levelCoord = [warp[2],warp[3]];
 				} else player.levelCoord[0]++;
 				player.x = 0;
 				player.y = blockSize*levels[player.currentLevel][0].findIndex(x => x[0]==-1 && x[1]==warpId)+(y1+blockSize)%blockSize;
 			} else if (y1 < 0) { // up
-				if (levels[player.currentLevel][coord[0]][coord[1]][2] != undefined) {
-					player.levelCoord = [coord[2],coord[3]];
+				if (warp[2] != undefined) {
+					player.levelCoord = [warp[2],warp[3]];
 				} else player.levelCoord[1]++;
 				player.y = levels[player.currentLevel][0].length * blockSize - playerSize;
 				player.x = blockSize*levels[player.currentLevel].findIndex(x => x[x.length-1][0]==-1 && x[x.length-1][1]==warpId)+(x1+blockSize)%blockSize;
 			} else if (y2 > levels[player.currentLevel][0].length * blockSize) { // down
-				if (levels[player.currentLevel][coord[0]][coord[1]][2] != undefined) {
-					player.levelCoord = [coord[2],coord[3]];
+				if (warp[2] != undefined) {
+					player.levelCoord = [warp[2],warp[3]];
 				} else player.levelCoord[1]--;
 				player.y = 0;
 				player.x = blockSize*levels[player.currentLevel].findIndex(x => x[0][0]==-1 && x[0][1]==warpId)+(x1+blockSize)%blockSize;
