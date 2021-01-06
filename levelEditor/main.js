d1e76b7f5ac67c0bb6d4743fb06972f810c4ca45
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-01-06 21:36:01 +0800

    Update main.js

--- levelEditor/main.js
+++ levelEditor/main.js
@@ -45,7 +45,7 @@ const blockName = ["Empty Space","Solid Block","Death Block","Check Point","Acti
 		   "S-Slow Field","S-Normal Field","S-Fast Field", // speed (21,22,23)
 		   "Bounce Block++","G-Bounce Up","G-Bounce Down", // more bounce (24,25,26)
 		   "Force Field L","Force Field R","Force Field U","Force Field D", // force (27,28,29,30)
-		   "Switch Block","Toggle Block A","Toggle Block B","Toggle Death Block A","Toggle Death Block B" // switchables (31,32,33,34,35)
+		   "Switch Block","Toggle Block A","Toggle Block B","Toggle Death Block A","Toggle Death Block B", // switchables (31,32,33,34,35)
 		   "Timer Block A","Timer Block B","Timer Death Block A","Timer Death Block B"]; // timer (36,37,38,39)
 const bannedBlock = [4,19,20];
 
