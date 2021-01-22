01078075f9db37c37ae7364e81ce6f12615ed640
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-01-22 14:13:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -720,9 +720,9 @@ function load() {
 	if (localStorage.getItem('just-a-save')) {
 		let saveData = JSON.parse(localStorage.getItem('just-a-save'));
 		if (saveData[5] == "Infinity") saveData[5] = Infinity;
-		if (saveData[6] == undefined) {
-			saveData[6] = newSave()[6];
-			saveData[3] += 3;
+		if (saveData[8] == undefined) {
+			saveData[8] = newSave()[6];
+			saveData[8] += 3;
 		}
 		player.spawnPoint = saveData;
 	}
