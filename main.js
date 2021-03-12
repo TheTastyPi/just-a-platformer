97f24ea72f3603c578b5d16f868db4f1f62e77ad
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-03-12 08:53:59 +0800

    fix bug where play turns black

--- main.js
+++ main.js
@@ -750,7 +750,7 @@ function newSave() {
   return [1, 6, 0, 8, 325, 1, 600, [], currentVersion];
 }
 function save() {
-  let saveData = player.spawnPoint;
+  let saveData = deepCopy(player.spawnPoint);
   if (saveData[5] == Infinity) saveData[5] = "Infinity";
   localStorage.setItem("just-a-save", JSON.stringify(saveData));
 }
@@ -856,6 +856,19 @@ function getBlockType(x, y) {
     return 1;
   }
   return level[x][y];
+}
+function deepCopy(inObject) {
+  //definitely not copied from somewhere else
+  let outObject, value, key;
+  if (typeof inObject !== "object" || inObject === null) {
+    return inObject;
+  }
+  outObject = Array.isArray(inObject) ? [] : {};
+  for (key in inObject) {
+    value = inObject[key];
+    outObject[key] = deepCopy(value);
+  }
+  return outObject;
 }
 function arraysEqual(a, b) {
   if (a === b) return true;
